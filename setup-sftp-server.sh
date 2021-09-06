a66c0e8a1fbca71e681c0307b45600c1ecbf9ba8
Mohammad Badiyu Zama <zamamb@gmail.com>
2021-09-06 10:41:37 +0530

    Update setup-sftp-server.sh

--- setup-sftp-server.sh
+++ setup-sftp-server.sh
@@ -1,3 +1,5 @@
+# Installation
+
 sudo apt update
 sudo apt install vsftpd
 sudo systemctl start vsftpd
@@ -11,5 +13,21 @@ sudo passwd ftpuser
 sudo ufw allow 20/tcp
 sudo ufw allow 21/tcp
 
+sudo ftp <host-name or ip address>
 
-sudo ftp ubuntu-ftp
+# Setup ssl 
+Add these to: /etc/vsftpd.conf
+ssl_enable=YES
+rsa_cert_file=/etc/letsencrypt/live/domain.com/fullchain.pem
+rsa_private_key_file=/etc/letsencrypt/live/domain.com/privkey.pem
+allow_anon_ssl=NO
+force_local_data_ssl=YES
+force_local_logins_ssl=YES
+ssl_tlsv1=YES
+ssl_sslv2=NO
+ssl_sslv3=NO
+require_ssl_reuse=NO
+ssl_ciphers=HIGH
+pasv_min_port=40000
+pasv_max_port=50000
+
