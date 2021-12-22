22dee6d9ed8d1ac4535b13542266ca542d546182
Mohammad Badiyu Zama <zamamb@gmail.com>
2021-12-22 11:34:18 +0530

    Create git-setup.sh

---
+++ git-setup.sh
@@ -0,0 +1,16 @@
+git checkout -b development
+git checkout --track origin/development
+git branch -vv
+ls -l
+git branch -u origin/development
+git branch -vv
+git pull
+history
+git config --global user.email "git-contractor1@effigo.tech"
+git config --global user.name "git-contractor1"
+git pull
+git reset --hard origin
+git pull
+git pull --hard origin/development
+git pull --set-upstream origin/development
+git status
